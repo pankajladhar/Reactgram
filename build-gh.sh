556c522895b47844c98a1d9a29e26cf917471e27
pankajladhar <ladharpankaj@gmail.com>
2017-02-13 23:34:43 +0530

    Added a script to automate pushing to gihthub

--- build-gh.sh
+++ build-gh.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+if [ -d "./images" ]
+then
+    rm -rf images
+fi
+if [ -d "./styles" ]
+then
+    rm -rf styles
+fi
 mv ./build/index.html index.html
 mv ./build/images/ images
 mv ./build/bundle.js bundle.js
 mv ./build/styles/ styles
+
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+git add .
+git commit -am "Deployed at $today"
+git push origin gh-pages
